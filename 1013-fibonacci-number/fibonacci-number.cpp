e9aee88250ec3d9b6b3eece97a84920a0c26c5d5
Riya <149486204+r-iya@users.noreply.github.com>
2026-02-13 16:12:58 +0530

    Time: 2 ms (78.22%) | Memory: 7.8 MB (36.23%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dp = vector<int>(31, -1);
+    
+
+    int fib(int n) {
+
+
+        if(n <= 1) return n;
+
+        if(dp[n]!= -1){
+            return dp[n];
+        }
+        return dp[n] = fib(n-1) + fib(n-2);
+    }
+};
