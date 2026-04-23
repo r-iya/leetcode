533c2887d5de3eb3365045c6cdd151179deb2b95
Riya <149486204+r-iya@users.noreply.github.com>
2026-04-23 09:32:46 +0530

    Time: 3 ms (73.84%) | Memory: 48.6 MB (61.16%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,19 +1,38 @@
+// class Solution {
+// public:
+//     vector<int> findDuplicates(vector<int>& nums) {
+//         unordered_map <int, int> mp;
+//         vector <int> ans;
+
+//         for(int i=0; i<nums.size(); i++){
+//             mp[nums[i]]++;
+//         }
+
+//         for(int i=0; i<nums.size(); i++){
+//             if(mp[nums[i]]==2){
+//                 ans.push_back(nums[i]);
+//                 mp[nums[i]] = 0;
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
-public:
-    vector<int> findDuplicates(vector<int>& nums) {
-        unordered_map <int, int> mp;
-        vector <int> ans;
+    public:
+        vector<int> findDuplicates(vector<int> & nums){
+            vector<int> ans;
 
-        for(int i=0; i<nums.size(); i++){
-            mp[nums[i]]++;
-        }
+            for(int i=0; i<nums.size(); i++){
+                int index = abs(nums[i]) -1;
 
-        for(int i=0; i<nums.size(); i++){
-            if(mp[nums[i]]==2){
-                ans.push_back(nums[i]);
-                mp[nums[i]] = 0;
+                if(nums[index] < 0){
+                    ans.push_back(abs(nums[i]));
+                }
+                else {
+                    nums[index] = -nums[index];
+                }
             }
+            return ans;
+
         }
-        return ans;
-    }
 };
